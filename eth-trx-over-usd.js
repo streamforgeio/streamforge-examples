f1ec9d233e7a3e009c60b83f5e8325aef4650997
ilker.ernas <ilker.ernas@verapi.com>
2019-05-05 13:33:50 +0300

    single ico samples are added.

---
+++ eth-trx-over-usd.js
@@ -0,0 +1,29 @@
+const sf = require('streamforge');
+
+var p = sf.Pipeline("ico-parity-compare").withComponent(
+	sf.Zip("ethereum-trx-calculation")
+	.withProcess(function(p1, p2) {
+		var r = {
+			'amount': (p1.amount * p2.body.result.price.last)
+		}
+        return {"request" : JSON.stringify(r)};
+	})
+	.withSource(
+		sf.Source("eth-pending", sf.DataSourceType.GLOBAL).withConflation(function(s1,s2){
+			print("eth-txId:" + s2.txId );
+			return {'amount': (s1.amount + s2.amount),'txId':'total' }
+		})
+	)
+	.withSource(
+		sf.Source("ico-parity", sf.DataSourceType.GLOBAL, function(s) {
+			return 	s.market == 'kraken' &&
+					s.ico == 'eth' &&
+				s.currency == 'usd';
+		})
+	).toSink(sf.APISink("api-compare","http://jumphost.streamforge.io:8080/api/trxs",
+    {   "http.method":"POST",
+        "http.api-key":"8d77f7d14a4864931f15072255fc1b58de8941cd45a8a896ed4ebf99b93d2e33"}))
+)
+
+//console.log(JSON.stringify(p));
+p.compile();
