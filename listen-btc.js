7e09a880d4c2718bf25cf50b999caed65251a494
ilker.ernas <ilker.ernas@verapi.com>
2019-05-14 22:53:30 +0200

    filter method is added.

--- listen-btc.js
+++ listen-btc.js
@@ -1,5 +1,6 @@
 const sf = require('streamforge');
 
+console.log('bisi')
 var p = sf.Pipeline("total-btc-trx-1-min").withComponent(
 	sf.Flow("bitcoin-trx-listener")
 	.withProcess(function(p1) {
@@ -10,7 +11,9 @@ var p = sf.Pipeline("total-btc-trx-1-min").withComponent(
 		return {"request" : JSON.stringify(r)};
 	})
 	.withSource(
-		sf.Source("btc-raw", sf.DataSourceType.GLOBAL)
+		sf.Source("btc-raw", sf.DataSourceType.GLOBAL,function(s) {
+			return 	s.amount > 1;
+		})
 	).toSink(sf.APISink("api-compare","http://jumphost.streamforge.io:8080/api/trxs",
     {   "http.method":"POST",
         "http.api-key":"8d77f7d14a4864931f15072255fc1b58de8941cd45a8a896ed4ebf99b93d2e33"}))
