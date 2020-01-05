cdf0990194e1a45841ff60f5f32f5621e71d4027
ilker.ernas <ilker.ernas@verapi.com>
2020-01-05 20:34:55 +0100

    top trx calculation is added.

--- eth-trx-over-usd.js
+++ eth-trx-over-usd.js
@@ -3,18 +3,41 @@ const sf = require('streamforge');
 var p = sf.Pipeline("ico-parity-compare").withComponent(
 	sf.Zip("ethereum-trx-calculation")
 	.withProcess(function(p1, p2) {
-		var r = {
-			'amount': (p1.amount * p2.body.result.price.last)
+		var finalTrxs = [];
+		if (p1.trxs != undefined){
+			p1.trxs.sort(function (a,b) {
+				if (a.amount > b.amount) {
+					return -1;
+				}
+				if (b.amount > a.amount) {
+					return 1;
+				}
+				return 0;
+			})
+			finalTrxs = p1.trxs.slice(0,10);
+		} else {
+			finalTrxs.push({'id' : p1.txId, 'amount' : p1.amount})
 		}
-		logger.info('amount : ' + r.amount);
-		//print("amount:"  +p1.amount );
-        return {"request" : JSON.stringify(r)};
+		
+
+		var r = {
+			'amount': (p1.amount * p2.body.result.price.last),
+			'topTrxs' : finalTrxs
+		}
+        return {"request" : JSON.stringify(r) };
 	})
 	.withSource(
 		sf.Source("eth-pending", sf.DataSourceType.GLOBAL).withConflation(function(s1,s2){
 			//print("s1 amount:"  + s1.txId  + " amount:" + s1.amount);
 			//print("s2 amount:"  + s2.txId  + " amount:" + s2.amount);
-			return {'amount': (s1.amount + s2.amount),'txId':'total' }
+			var trxs = [];
+			if (s1.trxs == undefined){
+				trxs.push({'id' : s1.txId, 'amount' : s1.amount});
+			} else {
+				trxs = s1.trxs;
+			}
+			trxs.push({'id' : s2.txId, 'amount' : s2.amount});
+			return {'amount': (s1.amount + s2.amount),'txId':'total','trxs' : trxs};
 		})
 	)
 	.withSource(
